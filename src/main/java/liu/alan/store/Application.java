6263e880bbb99a2be2190f5a571e141525aad7a3
Liu Guangyu <liuguangyu@LiutekiMBP.fritz.box>
2016-10-02 01:29:10 +1300

    Clean useless codes.

--- src/main/java/liu/alan/store/Application.java
+++ src/main/java/liu/alan/store/Application.java
@@ -1,7 +1,5 @@
 package liu.alan.store;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,15 +8,8 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        @SuppressWarnings("unused")
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/liu/alan/store/RestServiceController.java
+++ src/main/java/liu/alan/store/RestServiceController.java
@@ -1,6 +1,4 @@
 package liu.alan.store;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -9,18 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestServiceController {
 	@Autowired
 	private InventoryModel inventoryModel;
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Alan Liu!";
-    }
     
     @GetMapping("/inventory.json")
     public ProductAmount[] inventory(){
